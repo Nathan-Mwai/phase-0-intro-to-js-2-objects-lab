98628f6ca5653012e8d230b9e2b44b22832fb519
Nathan Mwai <nathan.mwai@student.moringaschool.com>
2024-06-26 16:06:39 +0300

    Adds index.js

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Write your solution in this file!
+function updateEmployeeWithKeyAndValue(employee, key, value){
+    const newEmployee = {...employee};
+    newEmployee[key] = value;
+    return newEmployee;
+  }
+
+const employee = {
+  name: "Sam",
+  streetAddress: "11 Broadway",
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value){
+    employee[key] = value;
+    return employee;
+  }
+
+ function deleteFromEmployeeByKey(employee, key){
+    const removeEmployee = {...employee}
+    delete removeEmployee[key];
+    return removeEmployee
+}
+
+function destructivelyDeleteFromEmployeeByKey(employee, key){
+    delete employee[key];
+    return employee
+}
